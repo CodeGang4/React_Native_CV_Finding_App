fe83d628ad72f7b8afb81caa807962eef47cc186
MinhNhat2710 <nhatdo2004@gmail.com>
2025-10-10 00:25:41 +0700

    Update main.jsx

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -4,17 +4,6 @@ import App from './App.jsx'
 import 'antd/dist/reset.css'
 import './index.css'
 
-// Import database test cho development
-if (import.meta.env.DEV) {
-  import('./utils/databaseTest.js').then(({ testDatabaseConnection }) => {
-    // Chạy test sau 2 giây để đảm bảo app đã load
-    setTimeout(() => {
-      console.log('🚀 Running database schema validation...')
-      testDatabaseConnection()
-    }, 2000)
-  })
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
